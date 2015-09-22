20df596198d58197b8380dd3d8fabc00ab495464
Mizki SUZUMORI <mizki9577@gmail.com>
2015-09-22 16:31:42 +0900

    get beautiful

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ setuptools.setup(
     author='Mizki SUZUMORI <mizki9577@gmail.com>',
     license='2-Clause BSD',
     packages=['anondbot'],
-    install_requires=['requests', 'requests_oauthlib', 'BeautifulSoup4', 'pep3143daemon'],
+    install_requires=[
+        'requests',
+        'requests_oauthlib',
+        'BeautifulSoup4',
+        'pep3143daemon'
+    ],
     entry_points={
         'console_scripts': [
             'anondbotd=anondbot:main'
